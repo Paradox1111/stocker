3258a77647dafa2c1989938af3a31eff836e6a6d
SRBond <skyler.bond7@gmail.com>
2020-01-25 13:51:15 -0800

    Add Router to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import Stocker from "./Stocker";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Stocker />, document.getElementById("root"));
+ReactDOM.render(
+	<Router>
+		<Stocker />
+	</Router>,
+	document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
